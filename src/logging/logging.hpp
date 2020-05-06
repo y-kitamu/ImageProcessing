7e2d0b8bf7a4c3e7ceb61ce57911684bbd884582
Kitamura <ymyk6602@gmail.com>
2020-05-06 15:28:20 +0900

    update logging

--- src/logging/logging.hpp
+++ src/logging/logging.hpp
@@ -9,28 +9,35 @@
 #ifndef LOGGING_HPP__
 #define LOGGING_HPP__
 
+#include <iostream>
 #include <chrono>
 #include <ctime>
-#include <fmt/format.h>
+#include <regex>
 
+#include <fmt/format.h>
 
 namespace logging {
 
+#ifndef logd
+#define logd(...) logging::_logd(__FILE__, __LINE__, __VA_ARGS__)
+#endif
+
 template <typename S, typename... Args>
-void logd(const S& format_str, const Args&... args) {
+void _logd(std::string filename, int line, const S& format_str, const Args&... args) {
     /*
      * debug log
      */
+    static const std::regex re(".*\n");
+
     auto now = std::chrono::system_clock::now();
     std::time_t current_time = std::chrono::system_clock::to_time_t(now);
-
-    char mbstr[19];
+    char mbstr[20];
     std::strftime(mbstr, sizeof(mbstr), "%Y-%m-%d %H:%M:%S", std::localtime(&current_time));
-    fmt::print("[{}] ({} at line {}) ", std::string(mbstr), __FILE__, __LINE__);
-    if (format_str[-1] == "\n") {
-        fmt::print(format_str, args...);
-    } else {
-        fmt::print(format_str + "\n", args...);
+
+    fmt::print("[{} ({} at line {})] ", mbstr, filename, line);
+    fmt::print(format_str, args...);
+    if (!std::regex_match(format_str, re)) {
+        std::cout << std::endl;
     }
 }
 
